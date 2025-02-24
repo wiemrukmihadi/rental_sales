f2444065710a3ea8c0411c894dce8f2221d769c5
Wiem Rukmihadi <wiemrukmihadi@jukesolutions.com>
2025-02-24 12:06:32 +0700

    update

--- models/product_template.py
+++ models/product_template.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import _, models, fields, api
 
 
 class ProductTemplate(models.Model):
@@ -14,4 +14,15 @@ class ProductTemplate(models.Model):
         rent_count = self.env['product.template'].search_count([('is_rent', '=', True)])
         for record in self:
             record.count_rent = rent_count
+
+    def action_rental_products(self):
+        return {
+            "name": _("Rental Product"),
+            "type": "ir.actions.act_window",
+            "view_mode": "tree,form",
+            "res_model": "product.template",
+            "target": "current",
+            "domain": [("is_rent", "=", True)],
+            "context": {"default_is_rent": self.id}
+        }
 

42003c8fd9cbf7321b79669e854cda2082980000
Wiem Rukmihadi <wiemrukmihadi@jukesolutions.com>
2025-02-25 18:09:45 +0700

    update

--- models/product_template.py
+++ models/product_template.py
@@ -9,16 +9,6 @@ class ProductTemplate(models.Model):
     is_rent  = fields.Boolean(string='Can be rented')
     count_rent  = fields.Integer(compute="_compute_count_rent")
 
-    rented_order_lines = fields.One2many(
-        "sale.order.line", "product_template_id", string="Rented Order Lines", compute="_compute_rented_orders"
-    )
-
-    # @api.depends('is_rent')
-    # def _compute_count_rent(self):
-    #     rent_count = self.env['product.template'].search_count([('is_rent', '=', True)])
-    #     for record in self:
-    #         record.count_rent = rent_count
-
     @api.depends('is_rent')
     def _compute_count_rent(self):
         for record in self:
@@ -27,14 +17,6 @@ class ProductTemplate(models.Model):
                 ('order_id.rental_status', '=', 'reserved')
             ])
 
-    @api.depends('is_rent')
-    def _compute_rented_orders(self):
-        for record in self:
-            record.rented_order_lines = self.env['sale.order.line'].search([
-                ('product_template_id', '=', record.id),
-                ('order_id.rental_status', '=', 'reserved')
-            ])
-    
     def action_view_reserved_orders(self):
         """Show Sale Orders where this product is rented and in Reserved status"""
         sale_orders = self.env['sale.order.line'].search([
